76b494893dd7168ed73222f1c6ddd5f8c9ee0667
lavezzo <lavezzo@t3desk019.mit.edu>
2021-12-06 12:58:04 -0500

    first version of generator that should work for SUEP files

--- ssd/generator.py
+++ ssd/generator.py
@@ -36,36 +36,16 @@ class CalorimeterJetDataset(torch.utils.data.Dataset):
         if not hasattr(self, 'hdf5_dataset'):
             self.open_hdf5()
 
-        idx_eFTrack_Eta = tcuda.LongTensor([self.eFTrack_Eta[index]],
+        idx_PFCand_Eta = tcuda.LongTensor([self.PFCand_Eta[index]],
                                            device=self.rank)
-        idx_eFTrack_Phi = tcuda.LongTensor([self.eFTrack_Phi[index]],
+        idx_PFCand_Phi = tcuda.LongTensor([self.PFCand_Phi[index]],
                                            device=self.rank)
-        val_eFTrack_PT = tcuda.FloatTensor(self.eFTrack_PT[index],
+        val_PFCand_PT = tcuda.FloatTensor(self.PFCand_PT[index],
                                            device=self.rank)
 
-        idx_eFPhoton_Eta = tcuda.LongTensor([self.eFPhoton_Eta[index]],
-                                            device=self.rank)
-        idx_eFPhoton_Phi = tcuda.LongTensor([self.eFPhoton_Phi[index]],
-                                            device=self.rank)
-        val_eFPhoton_ET = tcuda.FloatTensor(self.eFPhoton_ET[index],
-                                            device=self.rank)
-
-        idx_eFNHadron_Eta = tcuda.LongTensor([self.eFNHadron_Eta[index]],
-                                             device=self.rank)
-        idx_eFNHadron_Phi = tcuda.LongTensor([self.eFNHadron_Phi[index]],
-                                             device=self.rank)
-        val_eFNHadron_ET = tcuda.FloatTensor(self.eFNHadron_ET[index],
-                                             device=self.rank)
-
-        calorimeter, scaler = self.process_images(idx_eFTrack_Eta,
-                                                  idx_eFPhoton_Eta,
-                                                  idx_eFNHadron_Eta,
-                                                  idx_eFTrack_Phi,
-                                                  idx_eFPhoton_Phi,
-                                                  idx_eFNHadron_Phi,
-                                                  val_eFTrack_PT,
-                                                  val_eFPhoton_ET,
-                                                  val_eFNHadron_ET)
+        calorimeter, scaler = self.process_images(idx_PFCand_Eta,
+                                                  idx_PFCand_Phi,
+                                                  val_PFCand_PT)
         # Set labels
         labels = tcuda.FloatTensor(self.labels[index], device=self.rank)
         labels = self.process_labels(labels, scaler)
@@ -117,49 +97,30 @@ class CalorimeterJetDataset(torch.utils.data.Dataset):
         return tensor.sub(m).div(s)
 
     def open_hdf5(self):
+        print(self.source)
         self.hdf5_dataset = h5py.File(self.source, 'r')
 
-        self.eFTrack_Eta = self.hdf5_dataset['EFlowTrack_Eta']
-        self.eFTrack_Phi = self.hdf5_dataset['EFlowTrack_Phi']
-        self.eFTrack_PT = self.hdf5_dataset['EFlowTrack_PT']
-
-        self.eFPhoton_Eta = self.hdf5_dataset['EFlowPhoton_Eta']
-        self.eFPhoton_Phi = self.hdf5_dataset['EFlowPhoton_Phi']
-        self.eFPhoton_ET = self.hdf5_dataset['EFlowPhoton_ET']
-
-        self.eFNHadron_Eta = self.hdf5_dataset['EFlowNeutralHadron_Eta']
-        self.eFNHadron_Phi = self.hdf5_dataset['EFlowNeutralHadron_Phi']
-        self.eFNHadron_ET = self.hdf5_dataset['EFlowNeutralHadron_ET']
+        self.PFCand_Eta = self.hdf5_dataset['PFCand_Eta']
+        self.PFCand_Phi = self.hdf5_dataset['PFCand_Phi']
+        self.PFCand_PT = self.hdf5_dataset['PFCand_PT']
 
         self.labels = self.hdf5_dataset['labels']
-        self.base = self.hdf5_dataset['baseline']
+        
+        ### FIXME: ignored this for now
+        # self.base = self.hdf5_dataset['baseline']
 
         self.dataset_size = len(self.labels)
 
-    def process_images(self,
-                       idx_eFTrack_Eta, idx_eFPhoton_Eta, idx_eFNHadron_Eta,
-                       idx_eFTrack_Phi, idx_eFPhoton_Phi, idx_eFNHadron_Phi,
-                       val_eFTrack_PT, val_eFPhoton_ET, val_eFNHadron_ET):
-
-        v0 = 0*torch.ones(idx_eFTrack_Eta.size(1),
+    def process_images(self, idx_Eta, idx_Phi, idx_Pt):
+                
+        v0 = 0*torch.ones(idx_Eta.size(1),
                           dtype=torch.long).cuda(self.rank)
-        v1 = 1*torch.ones(idx_eFPhoton_Eta.size(1),
-                          dtype=torch.long).cuda(self.rank)
-        v2 = 2*torch.ones(idx_eFNHadron_Eta.size(1),
-                          dtype=torch.long).cuda(self.rank)
-
-        idx_channels = torch.cat((v0, v1, v2)).unsqueeze(0)
-        idx_eta = torch.cat((idx_eFTrack_Eta,
-                             idx_eFPhoton_Eta,
-                             idx_eFNHadron_Eta), 1)
-        idx_phi = torch.cat((idx_eFTrack_Phi,
-                             idx_eFPhoton_Phi,
-                             idx_eFNHadron_Phi), 1)
-
-        i = torch.cat((idx_channels, idx_eta, idx_phi), 0)
-        v = torch.cat((val_eFTrack_PT, val_eFPhoton_ET, val_eFNHadron_ET))
+        idx_channels = v0.unsqueeze(0)
+        i = torch.cat((idx_channels, idx_Eta, idx_Phi), 0)
+        v = idx_Pt
 
         scaler = torch.max(v)
+                
         pixels = torch.sparse.FloatTensor(i, v, torch.Size([self.channels,
                                                             self.width,
                                                             self.height]))
@@ -188,9 +149,14 @@ class CalorimeterJetDataset(torch.utils.data.Dataset):
         base = torch.cat((base, base_reshaped[:, 3].unsqueeze(1)), 1)
 
         return base
-
+    
     def process_labels(self, labels_raw, scaler):
-        labels_reshaped = labels_raw.reshape(-1, 4)
+                
+        labels_reshaped = labels_raw.reshape(-1, 5)
+        
+        ### FIXME: ignored the mass, for now
+        labels_reshaped = labels_reshaped[:,:4]
+        
         labels = torch.empty_like(labels_reshaped)
 
         # Set fractional coordinates
